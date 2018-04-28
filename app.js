6551c564cedb8d24280afed059f765812f5ec482
luke <azathothbeats@gmail.com>
2018-04-28 17:51:08 -0400

    App listens to port 3000

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log('diddle')
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('hey')
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000!')
+})

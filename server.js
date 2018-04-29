e9b5aaed3c174b9259b6e70afec17cbe632febe5
luke <azathothbeats@gmail.com>
2018-04-28 21:06:26 -0400

    Created base folder structure

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>mic check 1 2 1 2</h1>')
+  res.send('<h1>mic check retard</h1>')
+})
+
+app.get('*', (req, res) => {
+  res.send('<h1>Page not found.</h1>')
 })
 
 app.listen(3000, () => {
